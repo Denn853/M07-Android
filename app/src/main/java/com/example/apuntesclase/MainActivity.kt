112bd05713656d13618cf9a52bd20d662bfb5500
DennysHernandezMunis <dennys.hernandez@enti.cat>
2023-09-18 20:04:30 +0200

    For's type

--- app/src/main/java/com/example/apuntesclase/MainActivity.kt
+++ app/src/main/java/com/example/apuntesclase/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.apuntesclase
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.apuntesclase.ui.theme.ApuntesClaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +25,44 @@ class MainActivity : ComponentActivity() {
             ApuntesClaseTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    PrintOnScreen("dennys👽", "🦇")
+                    Column() {
+
+                        //For normal
+                        for (i in 1 .. 5) {
+                            PrintOnScreen("dennys 👽🦇")
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        //For each
+                        val names = listOf("Pepe", "Pepa", "Pedro", "Paula")
+
+                        for (name in names) {
+                            PrintOnScreen(name)
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        //
+                        for (i in names.indices) {
+                            PrintOnScreen("index: " + i.toString())
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        //For early acces
+                        names.forEach {name ->
+                            PrintOnScreen(name)
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        //For early acces
+                        names.forEachIndexed { index, name ->
+                            PrintOnScreen(name + " " + index.toString())
+                        }
+
+                    }
                 }
             }
         }
@@ -28,7 +70,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PrintOnScreen(name: String?, name2: String?, modifier: Modifier = Modifier) {
+fun PrintOnScreen(name: String, modifier: Modifier = Modifier) {
 
     /*val name = name?.let{
         val name2 = name2?.let {
@@ -58,4 +100,9 @@ fun PrintOnScreen(name: String?, name2: String?, modifier: Modifier = Modifier) 
         text = "Hello ${name.uppercase()} ${name2.uppercase()}",
         modifier = modifier
     )*/
+
+    Text(
+        text = "Hello ${name.uppercase()}",
+        modifier = modifier
+    )
 }
