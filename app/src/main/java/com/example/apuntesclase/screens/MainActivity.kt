4d450398e9e78d49e4b04cbd739d828848afda7b
DennysHernandezMunis <dennys.hernandez@enti.cat>
2023-10-16 20:54:26 +0200

    Starting Note's App

--- app/src/main/java/com/example/apuntesclase/screens/MainActivity.kt
+++ app/src/main/java/com/example/apuntesclase/screens/MainActivity.kt
@@ -1,30 +1,25 @@
 package com.example.apuntesclase.screens
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.apuntesclase.R
-import com.example.apuntesclase.heros.HeroAdapter
-import com.example.apuntesclase.heros.HeroData
-import com.example.apuntesclase.heros.HeroProvider
 
 class MainActivity : ComponentActivity() {
 
-    val table by lazy { findViewById<RecyclerView>(R.id.my_recyclerView) }
+    val btChangeScreen by lazy { findViewById<Button>(R.id.bt_addNote) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
 
-        table.layoutManager = LinearLayoutManager(this)
-        table.adapter = HeroAdapter(HeroProvider.GetAllHeros())
+        btChangeScreen.setOnClickListener {
+            val newIntent = Intent(this, SecondScreen::class.java)
+            newIntent.putExtra(SecondScreen.ExtraTextKey, "Cadena de texto transferida")
 
+            this.startActivity(newIntent)
+            overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left)
+        }
     }
 }
